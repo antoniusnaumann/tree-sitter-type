fd951ad46d771588f0512558d65b2588c84a3785
Antonius Naumann <git@antonius.dev>
2024-08-25 17:43:56 +0200

    Add highlighting rules

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,8 +1,11 @@
 ; highlighting rules follow the neovim standard highlight groups: https://neovim.io/doc/user/treesitter.html#treesitter-highlight
 
-(type_keyword) @type
+(type_keyword) @keyword.type
 
 (type_ident) @type
+((type_ident) @type.builtin
+  (#match? @type.builtin "^(String|Int|Int64|UInt|UInt64|USize|ISize|Int8|UInt8|Int16|UInt16|Int32|UInt32|Bool|Float|Double)$"))
+
 (ident) @variable.member
 (comment) @comment
 
